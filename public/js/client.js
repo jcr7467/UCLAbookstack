8cabac298c9270dee28efbd64e61a8a85a2a6d0e
jcr7467 <25701682+jcr7467@users.noreply.github.com>
2020-08-22 14:22:33 -0700

    Added issue templates. Implemented new messaging format with removal of bugs when leaving rooms, had to remove eventlisteners

--- public/js/client.js
+++ public/js/client.js
@@ -1,103 +1,7 @@
-
-/*
-
-
-const socket = io();
-const chatForm = document.getElementById('chat-form')
-const chatMessages = document.querySelector('.chat-messages')
-
-const myUserID = document.getElementById('myUserID').value
-const theirUserID = document.getElementById('theirUserID').value
-
-
-
-let userIDs = [myUserID, theirUserID]
-
-userIDs.sort();
-
-let room = userIDs[0].concat(userIDs[1])
-let username = myUserID;
-
-console.log(myUserID)
-console.log(theirUserID)
-
-
-//join chatroom
-socket.emit('joinRoom', {myUserID, theirUserID, room});
-
-socket.on('message', (message) => {
-
-    console.log(message);
-
-    outputMessage(message);
-
-    //scroll down
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-});
-
-socket.on('serverObject', (obj) => {
-    console.log("Server's response")
-    console.log(obj)
-
-})
-
-
-
-chatForm.addEventListener('submit', (e) => {
-
-
-   e.preventDefault();
-
-
-   const msg = e.target.elements.msg.value;
-   socket.emit('chatMessage', msg);
-
-   //Clear input
-    e.target.elements.msg.value = '';
-    e.target.elements.msg.focus();
-
-
-});
-
-
-
-
-function outputMessage(msg){
-    const div = document.createElement('div');
-
-    console.log('mine', myUserID)
-    console.log('msgcurrent', msg.currentUserid)
-
-    if (myUserID == msg.currentUserid){
-        div.classList.add('message');
-        div.innerHTML = `<p class="meta">${msg.currentUserfirstname} <span>${msg.messageObject.time}</span> <span>${msg.messageObject.date}</span></p>
-        <p class="text">
-            ${msg.messageObject.text}
-        </p>`;
-    }else{
-
-        div.classList.add('penpalmessage');
-        div.innerHTML = `<p class="meta">${msg.currentUserfirstname} <span>${msg.messageObject.time}</span> <span>${msg.messageObject.date}</span></p>
-        <p class="text">
-            ${msg.messageObject.text}
-        </p>`;
-
-    }
-
-
-
-    document.querySelector('.chat-messages').appendChild(div);
-
-}
-
-
-*/
-
-
 const socket = io();
 
-const chatform = $("#send-message-form");
-let chatmessages = $(".chat-messages-container")
+const chatform = $("#chat-form");
+let chatmessages = $("#chat-messages-container");
 
 let theirUserID,
     myUserID;
@@ -111,18 +15,18 @@ let outputAJAXMessages = (msgObj) => {
     //     thisuser: request.session.userId
     // }
 
-    $(".chat-messages-container").empty()
-    console.log(msgObj)
-    for (let i = 0; i < msgObj.messages.length; i++) {
 
-
+    $("#chat-messages-container .col-12").empty();
+    console.log(msgObj);
+    for(let i = 0 ; i < msgObj.messages.length ; i++){
         const messageContainerDiv = document.createElement('div');
         $(messageContainerDiv).addClass("chat-message-container")
+
 
         const messageDiv = document.createElement('div');
         $(messageDiv).addClass('chat-message');
 
-        if (msgObj.messages[i].msgSentByMe === myUserID)
+        if(msgObj.messages[i].msgSentByMe == msgObj.thisuserId)
             $(messageContainerDiv).addClass('sent');
         else
             $(messageContainerDiv).addClass('received');
@@ -134,14 +38,13 @@ let outputAJAXMessages = (msgObj) => {
         $("#chat-messages-container .col-12").append(messageContainerDiv);
 
         $('#theirUserID').attr('value', msgObj.myPenpal._id);
-
         $(".replace-name").text(msgObj.myPenpal.firstname);
-        $(".hideme").show()
-
+        $(".hideme").show();
     }
 
-
+    setupChatMessages();
 }
+
 
 function setupChatMessages() {
 //Chat messages setup
@@ -154,14 +57,13 @@ $(".person-container").click((event) => {
     myUserID = $(event.currentTarget).find(".myUserID").text();
 
 
+
     let userIDs = [myUserID, theirUserID]
     userIDs.sort();
     let room = userIDs[0].concat(userIDs[1])
 
     //join chatroom
     socket.emit('joinRoom', {myUserID, theirUserID, room});
-
-
 
     $.ajax({
         method: "POST",
@@ -171,70 +73,69 @@ $(".person-container").click((event) => {
 
         outputAJAXMessages(res, myUserID, theirUserID)
     });
-
-
-
-
-})
+});
 
 
 
 
 let outputSocketMessage = (msg) => {
-
     /* This is what the msg object looks like
-                    {
-                        msgObj: msgObj,
-                        currentUserid: response.data.currentUserid,
-                        currentUserfirstname: response.data.currentUserfirstname,
-                        penpalUserid: response.data.penpalUserId,
-                        penpalUserfirstname: response.data.penpalUserfirstname
-                    }
+                {
+                    msgObj: msgObj,
+                    currentUserid: response.data.currentUserid,
+                    currentUserfirstname: response.data.currentUserfirstname,
+                    penpalUserid: response.data.penpalUserId,
+                    penpalUserfirstname: response.data.penpalUserfirstname
+                }
     * */
 
 
-    const messageContainerDiv = document.createElement('div');
-    $(messageContainerDiv).addClass("chat-message-container")
-
-
-    const div = document.createElement('div');
-
-    //div.classList.add('message');
-
-
-    if(msg.msgObj.username === myUserID)
-        $(messageContainerDiv).addClass('sent');
-    else
-        $(messageContainerDiv).addClass('received');
-
+    // Create a new message div
+    const messageDiv = document.createElement('div');
+    $(messageDiv).addClass('chat-message');
     $(messageDiv).append(`<div class="message-body">${msg.msgObj.text}</div>
         <div class="message-date">${msg.msgObj.time} ${msg.msgObj.date}</div>`);
 
-    //This was used before to autoscroll on new message
+    // Create a container for the message div above and append the message to this container
+    const messageContainerDiv = document.createElement('div');
+    $(messageContainerDiv).addClass("chat-message-container")
+    $(messageContainerDiv).append(messageDiv);
+    if(msg.msgObj.username === myUserID){
+        $(messageContainerDiv).addClass('sent');
+    } else{
+        $(messageContainerDiv).addClass('received');
+    }
+
+    // Append the container above into the outermost container
+    $("#chat-messages-container .col-12").append(messageContainerDiv);
+    $('#chat-messages-container').scrollTop($('#chat-messages-container').prop('scrollHeight'));
+    setupChatMessages();
+
+
+    //This was used before to autoscroll on new message, now lays unused, here for future reference
     //chatmessages.scrollTop = chatmessages.scrollHeight;
 }
 
 
 
-$("#sendMessageButton").click(() => {
-    chatform.submit()
-})
+$(".sendMessageButton").click(() => {
+    chatform.submit();
+});
 
 
 chatform.submit((e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    let msg = $("#sendMessageInput")
+    let msg = $("#chat-input");
     //socket.emit('chatMessage', msg);
 
-    socket.emit('chatMessage', msg.val())
+    socket.emit('chatMessage', msg.val());
 
     //Clear input
-    msg.val('')
-    msg.focus()
+    msg.val('');
+    msg.focus();
 
-})
-
+});
 
 
 socket.on('serverToClientMessage', (message) => {
@@ -243,32 +144,23 @@ socket.on('serverToClientMessage', (message) => {
 
     //console.log(message)
     //This scrolls the messages window down when we get a new message
-    chatmessages.scrollTop = chatmessages.scrollHeight;
-})
-
-
-
-
-
-
+    $('#chat-messages-container').scrollTop($('#chat-messages-container').prop('scrollHeight'));
+});
 
 
 
 $(document).ready(
     function() {
-        // $('#formfilt').hide()
         //Shown in conversation list inside every person's name
-        $('.theirUserID').hide()
-        $('.myUserID').hide()
+        $('.theirUserID').hide();
+        $('.myUserID').hide();
 
 
         //Used in the row when user is about to send message
-        $('#theirUserID').hide()
-        $('#myUserID').hide()
-        $('.hideme').hide()
-        // chatMessages.scrollTop = chatMessages.scrollHeight;
+        $('#theirUserID').hide();
+        $('#myUserID').hide();
+        $('.hideme').hide();
     }
-
 );
 
 

cc090569e2f2d30c8e215280e27f2a81a5dbc78a
jcr7467 <25701682+jcr7467@users.noreply.github.com>
2020-10-01 01:01:37 -0700

    Added requirement for a new user to use their UCLA email

--- routes/account_routes.js
+++ routes/account_routes.js
@@ -75,6 +75,14 @@ router.route('/signup')
                 request.body.lastname){
 
 
+
+                /*We only want users with UCLA emails to be able to make an account, so no one can troll and spam our website*/
+                if (!(request.body.email.endsWith("@ucla.edu")) || request.body.email.endsWith("@g.ucla.edu")){
+                    let err = new Error("Please use your UCLA email (:")
+                    return next(err);
+                }
+
+
                 //Creates Javascript object with form input data
                 let userData = {
                     email: request.body.email,

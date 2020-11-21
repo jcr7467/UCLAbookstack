35dd7f1849eb425f69f7102578a0655654433181
Juan Rios <25701682+jcr7467@users.noreply.github.com>
2020-11-21 11:51:54 -0800

    Changed functionality when a user sends a new message from a post page. (#122)
    * Changed functionality when a user sends a new message from a post page.
    Now, instead of first redirecting them to the chat page, we send a message from that same page. This saves us a lot of trouble when sending a new message from the chat page.
    
    We also hide the chat buttom that sends us to the chat page from navbars until a user has at least 1 person they are talking to.
    
    * Removed 'new message' checking from the rendering of our chat page, '/conversations'
    
    * Removed check for 'bookowner', fixed bug that allowed unverified users to send messages, fixed bug that allowed null/empty/white-space messages to be sent. Added messages on chat page when no messages or unverified email.
    
    Removed check for bookOwner in HBS template, because of how we changed functionality for new messages, it no longer checks if we receive that parameter to create a new message.
    
    Fixed bugs for new-message modal/sending
    
    * Removed whitespace change, to avoid unnecessary git changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -26,6 +26,15 @@ $(document).ready(function(){
     }
 
     screenWidth = $(window).width();
+
+
+
+    //Forces the first person container to be clicked resulting in the message container being populated
+    //with the messages of the top person container so it is not empty
+    //If on mobile, screen width < 576px, this does not occur.
+    if ($(window).width() >= 576) {
+        $('.person-container').first().trigger('click');
+    }
 });
 
 
